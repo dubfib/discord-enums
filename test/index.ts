342eab03c90e1b496596f649e95a2542b1944bf8
dubfib <81378985+dubfib@users.noreply.github.com>
2023-01-20 19:24:27 -0800

    Add files via upload

---
+++ test/index.ts
@@ -0,0 +1,12 @@
+import { 
+    GatewayCloseCodes, 
+    GatewayDispatchEvents, 
+    GatewayIntentBits, 
+    GatewayOpcodes 
+} from '../index';
+
+//Gateway
+console.log(GatewayCloseCodes);
+console.log(GatewayDispatchEvents);
+console.log(GatewayIntentBits);
+console.log(GatewayOpcodes);

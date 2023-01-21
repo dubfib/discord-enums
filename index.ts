ee084d20fff1da33dd33c1861e9a25ea1ca12020
dubfib <81378985+dubfib@users.noreply.github.com>
2023-01-20 19:10:35 -0800

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,13 @@
+import {
+    GatewayIntentBits,
+    GatewayDispatchEvents,
+    GatewayCloseCodes,
+    GatewayOpcodes
+} from './src/Gateway';
+
+export {
+    GatewayIntentBits,
+    GatewayDispatchEvents,
+    GatewayCloseCodes,
+    GatewayOpcodes
+}
